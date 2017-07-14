04fc1baf39fcb99164ff09beeee453e7a20ffd42
Youngmagu <benjamin.t.munoz@gmail.com>
2017-07-14 10:35:33 -0500

    Procedural Generation Started For Game Jam

---
+++ procedurally generated/Assets/Scripts/Game Board.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum Tiles
+{
+    Floor,Wall,Crowd,Door,
+}
+
+public class GameBoard : MonoBehaviour {
+
+    public int numberOfRooms;
+
+    public int roomHeight = 11;// Height of the room
+    public int roomWidth = 11;// Width of the room
+
+    public int boardHeight = 1000; // Height of the board
+    public int boardWidth = 1000;// Width of the board
+    
+    public Room[][] Board;
+    public GameObject boardHolder;
+    
+     
+
+    // Use this for initialization
+    void Start () {
+
+        boardHolder = new GameObject("BoardHolder");
+
+        populateBoard();
+
+        
+        
+
+    }
+
+    void populateBoard()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ procedurally generated/Assets/Scripts/Room.cs
@@ -0,0 +1,181 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction
+{
+    North, East, South, West,
+}
+
+public class Room : MonoBehaviour {
+
+  private  Tiles[][] tiles;
+    private int exitX = 0;
+    private int exitY = 0;
+    private int enterX = 0;
+    private int enterY = 0;
+
+    public void SetupRoom(int width, int height, int columns, int row, Direction exit)
+    {
+        tiles = new Tiles[width][];
+
+        // Go through all the tile arrays...
+        for (int i = 0; i < tiles.Length; i++)
+        {
+            // ... and set each tile array is the correct number of rows.
+            tiles[i] = new Tiles[height];
+        }
+
+
+        switch (exit)
+        {
+            case Direction.North:
+                exitX= width/2;
+                exitY = height-1;                 
+                break;
+            case Direction.East:
+                exitX = width -1;
+                exitY = height/2;
+                break;
+
+            case Direction.South:
+                exitX = width /2;
+                exitY = 0;
+                break;
+
+            case Direction.West:
+                exitX = 0;
+                exitY = height / 2;
+                break;
+        }
+
+        for (int j = 0; j < tiles.Length; j++)
+        {
+            for (int k = 0; k < tiles.Length; k++)
+            {
+
+                if(j==exitX && k==exitY )
+                {
+                    tiles[j][k] = Tiles.Door;
+                }
+
+               else if(j==0||j==width-1)
+                {
+                    tiles[j][k] = Tiles.Wall;
+                }
+
+                else if (k == 0 || k == height - 1)
+                {
+                    tiles[j][k] = Tiles.Wall;
+                }
+
+                else
+                {
+                    tiles[j][k] = Tiles.Floor;
+                }
+
+            }
+        }
+
+
+    }
+
+    public void SetupRoom(int width, int height, int columns, int rows, Direction enterance, Direction exit)
+    {
+
+        tiles = new Tiles[width][];
+
+        // Go through all the tile arrays...
+        for (int i = 0; i < tiles.Length; i++)
+        {
+            // ... and set each tile array is the correct number of rows.
+            tiles[i] = new Tiles[height];
+        }
+
+
+        switch (exit)
+        {
+            case Direction.North:
+                exitX = width / 2;
+                exitY = height - 1;
+                break;
+            case Direction.East:
+                exitX = width - 1;
+                exitY = height / 2;
+                break;
+
+            case Direction.South:
+                exitX = width / 2;
+                exitY = 0;
+                break;
+
+            case Direction.West:
+                exitX = 0;
+                exitY = height / 2;
+                break;
+        }
+
+        switch (enterance)
+        {
+            case Direction.North:
+                enterX = width / 2;
+                enterY = height - 1;
+                break;
+            case Direction.East:
+                enterX = width - 1;
+                enterY = height / 2;
+                break;
+
+            case Direction.South:
+                enterX = width / 2;
+                enterY = 0;
+                break;
+
+            case Direction.West:
+                enterX = 0;
+                enterY = height / 2;
+                break;
+        }
+
+        for (int j = 0; j < tiles.Length; j++)
+        {
+            for (int k = 0; k < tiles.Length; k++)
+            {
+
+                if (j == exitX && k == exitY) // checks to see if the tile is on the exit
+                {
+                    tiles[j][k] = Tiles.Door;
+                }
+
+                else if (j == enterX && k == enterY) // checks to see if the tile is on the entrance
+                {
+                    tiles[j][k] = Tiles.Door;
+                }
+
+                else if (j == 0 || j == width - 1) // Top or bottom of the array
+                {
+                    tiles[j][k] = Tiles.Wall;
+                }
+
+                else if (k == 0 || k == height - 1) // Left or right of the the array
+                {
+                    tiles[j][k] = Tiles.Wall;
+                }
+
+                else
+                {
+                    tiles[j][k] = Tiles.Floor;
+                }
+
+            }
+        }
+
+
+    }
+
+}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
